8f2124452cd569282d8ec57b8943410ec044082e
LeeJaeEun0 <wodmschlrh12@naver.com>
2023-04-18 01:32:07 +0900

    use img

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import {Button , Nav, Navbar, Container} from 'react-bootstrap';
 import './App.css';
+import  bg from './img/bg.png'; // import 작명 from '이미지 경로'
 
 function App() {
   return (
@@ -17,8 +18,33 @@ function App() {
         </Container>
       </Navbar>
 
-    <div className='main-bg'></div>
+    {/* <div className='main-bg'></div> */}
+    {/* html에서 이미지 넣을 경우 -> import 필요함 */}
+    <div className='main-bg' style={{backgroundImage : 'url('+bg+')'}}></div>
 
+    {/* 상품 레이아웃 3개 만들기 */}
+    <div className="container">
+  <div className="row">
+    {/* 첫번째 상품 */}
+    <div className="col-md-4">
+      <img src="https://codingapple1.github.io/shop/shoes1.jpg" width='80%'/>
+      <h4>상품명</h4>
+      <p>상품 설명</p>
+    </div>
+    {/* 두번째 상품 */}
+    <div className="col-md-4">
+    <img src="https://codingapple1.github.io/shop/shoes2.jpg" width='80%'/>
+      <h4>상품명</h4>
+      <p>상품 설명</p>
+    </div>
+    {/* 세번째 상품 */}
+    <div className="col-md-4">
+    <img src="https://codingapple1.github.io/shop/shoes3.jpg" width='80%'/>
+      <h4>상품명</h4>
+      <p>상품 설명</p>
+    </div>
+  </div>
+</div> 
     </div>
   );
 }
