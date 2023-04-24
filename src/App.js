b5131727d1acaa19e7617702498fc3455468bffa
LeeJaeEun0 <wodmschlrh12@naver.com>
2023-04-24 11:41:57 +0900

    axios를 통해 ajax 사용하기

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { useState } from "react";
 import data from "./data.js";
 import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom"; // Router 사용하기 위해서 import
 import Detail from "./routes/Detail.js";
-
+import axios from "axios";
 
 function App() {
   let [shoes, shoeschange] = useState(data);
@@ -63,13 +63,36 @@ function App() {
                     return <Card shoes={shoes[i]} i={i + 1}></Card>;
                   })}
                 </div>
+                <button
+                  onClick={() => {
+                    axios
+                      .get("https://codingapple1.github.io/shop/data2.json")
+                      .then((결과) => {
+                        let copy = [...shoes, ...결과.data]
+                        shoeschange(copy)
+                      })
+                      .catch(() => { // 실패한 경우 보일 것
+                        console.log("실패함");
+                      });
+
+                    axios.post('/safdfas', {name: 'kim'});
+                    
+                    // 동시에 ajax 요청 여러개 하면(여러곳에서 get을 하던가)
+                    Promise.all(axios.get('/url1'),axios.get('/url2')).then(() =>{
+                      // 두개 성공했을 경우 실행할 코드
+                    })
+                    // array와 object 데이터를 받을 수 있는 이유: json
+
+                  }}
+                >
+                  버튼
+                </button>
               </div>
             </>
           }
         />
         <Route path="/detail" element={<Detail />} />
         <Route path="*" element={<div>없는 페이지 404에러</div>} />{" "}
-
         {/*지정한 페이지 이외의 모든 값을 입력하면 오류 발생 */}
         {/* <Route path='/about' element={<div>어바웃페이지</div>}/> */}
         {/* 여러 페이지에 같은 html 태크를 출력하고 싶은 경우 */}
@@ -77,19 +100,16 @@ function App() {
         <Route path='/about/member' element={<About/>}/>
         <Route path='/about/location' element={<About/>}/> */}
         {/* tag 안의 태그: nested route */}
-
         <Route path="/about" element={<About />}>
           <Route path="member" element={<div>멤버임</div>} />
           <Route path="location" element={<div>위치 정보임</div>} />
         </Route>
-
-        <Route path="/event" element={<Event/>}>
-          <Route path="one" element={<p>첫 주문시 양배추즙 서비스</p>}/>
-          <Route path="two" element={<p>생활기념 쿠폰 받기</p>}/>
+        <Route path="/event" element={<Event />}>
+          <Route path="one" element={<p>첫 주문시 양배추즙 서비스</p>} />
+          <Route path="two" element={<p>생활기념 쿠폰 받기</p>} />
         </Route>
-
         {/* 페이지를 여러개 만들고 싶으면 :URL 파라미터 만들수 있음 */}
-        <Route path="/detail/:id" element={<Detail shoes={shoes}/>}></Route>
+        <Route path="/detail/:id" element={<Detail shoes={shoes} />}></Route>
       </Routes>
     </div>
   );
